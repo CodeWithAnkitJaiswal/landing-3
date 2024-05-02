051fb471765a3d87998b2017a9e88d7d0eeeae2f
Ankit Jaiswal <78140180+CodeWithAnkitJaiswal@users.noreply.github.com>
2024-05-02 18:56:51 +0530

    Add files via upload

--- try.js
+++ try.js
@@ -12,3 +12,55 @@ parentElement.addEventListener('click', function(e) {
             image.src = '/images/icon-minus.svg'
     }
 })
+
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('.slide a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// to open tg GTC from logo
+function openGTC() {
+    window.location.href = '';
+}
+
+function openMains() {
+    window.location.href = '';
+}
+
+function openAdv() {
+    window.location.href = '';
+}
+
+function openCsab() {
+    window.location.href = '';
+}
+
+function openOth() {
+    window.location.href = '';
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const coffeeButton = document.getElementById("coffee-button");
+
+    // Expand the button after 20 seconds
+    setTimeout(function() {
+        coffeeButton.classList.add("expanded");
+    }, 8000);
+
+    // Contract the button after 30 seconds
+    setTimeout(function() {
+        coffeeButton.classList.remove("expanded");
+    }, 20000);
+});
